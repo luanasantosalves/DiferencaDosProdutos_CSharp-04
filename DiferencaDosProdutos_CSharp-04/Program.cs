08ea68b7b68ba9e0460573724f2d2b473f8361fa
Luana <Luana@DESKTOP-ODA6GJK>
2022-04-21 17:42:28 -0300

    Adicionar arquivos de projeto.

---
+++ DiferencaDosProdutos_CSharp-04/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DiferencaDosProdutos_CSharp_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int A, B, C, D, DIFERENCA;
+
+            Console.WriteLine("Digite os valores de A, B, C e D:");
+            A = int.Parse(Console.ReadLine());
+            B = int.Parse(Console.ReadLine());
+            C = int.Parse(Console.ReadLine());
+            D = int.Parse(Console.ReadLine());
+
+            DIFERENCA = A * B - C * D;
+
+            Console.WriteLine("A diferença dos produtos é: " + DIFERENCA);
+        }
+    }
+}
